66bb218901a859cce9ae5be9677abddedd00297f
tkendalls <director@mathplusacademy.com>
2015-07-11 12:34:47 -0400

    update

--- functions.rb
+++ functions.rb
@@ -13,7 +13,15 @@ class Functions
     while number = numbers.pop
       sum += number
     end
+    sum
+  end
 
+  def sum_recursion(numbers, sum=0)
+    sum += numbers.pop
+
+    if numbers
+      sum_recursion(numbers, sum)
+    end
     sum
   end
 end
